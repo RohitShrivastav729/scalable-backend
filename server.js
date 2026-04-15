e7ca3370fcd331532399d33268dde4afcca23197
Rohit Shrivastav <rohit-notifi@Rohits-MacBook-Air.local>
2026-04-15 22:19:37 +0530

    health route updated

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("Scalable Backend Running..");
+    res.status(200).send("Scalable Backend Running..");
 });
 
 const authRoutes = require("./src/routes/authRoutes");
